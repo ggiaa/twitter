2722a36ab2b031648363de5d1e3d0e3189da104c
Gia <giaa601@gmail.com>
2022-03-16 18:58:56 +0700

    desain timeline

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,6 +1,29 @@
 <x-app-layout>
     <div class="row col-lg-12">
         <div class="col-lg-8">
+
+            <form action="" method="POST">
+                <div class="border rounded-3">                
+                    <div class="d-flex mt-3">                
+                        <div class="flex-shrink-0 mr-3">
+                            <img class="w-10 h-10 rounded-circle" style="height: 40px; width: 40px; margin-inline-end: 10px; margin-inline-start: 10px" src="https://i.pravatar.cc/150" alt="{{ Auth::user()->name }}">
+                        </div>
+                        <div class="w-100" >
+                            <div class="mb-2" style="font-weight: 600">
+                                {{ Auth::user()->name }}
+                            </div>
+                            
+                            <div class="mb-3">                                
+                                <textarea id="my-textarea" rows="3" placeholder="What is on your mind..." class="form-control" name="" style="resize: none; width: 95%"></textarea>
+                            </div>
+                            <div class="d-flex justify-content-end mb-3"  style="width: 95%">
+                                <button class="btn btn-primary">Send</button>
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
+            </form>
+
             <div class="border rounded-3">
                 @foreach ($statuses as $status)
                 <div class="d-flex mt-3">                
